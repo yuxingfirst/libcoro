df5060e81aaf60af97462bf84d4a5e292180e5e8
herry-xiong <yuxingfirst@gmail.com>
2013-12-23 19:19:57 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
-
 #include "coro.h"
 
 coro_context ctx, mainctx;
-struct coro_stack stack;
-
-int count = 0;
-
+struct coro_stack mainstack, stack;
+long count = 0;
 void coro_body(void *arg)
 {
     while(1) {
       count++;
       coro_transfer(&ctx, &mainctx);
+      //printf("coro:%d\n", count);
     }
 }
 
 int main(int argc, char **argv)
 {
-    coro_create(&mainctx, NULL, NULL, NULL, 0);
-    coro_stack_alloc(&stack, 0);
+    long counter = atol(argv[1]);
+    int stacksize = atoi(argv[2]);
+    printf("total:%ld, stackszie:%dKB\n", counter, stacksize);
+    coro_stack_alloc(&mainstack, stacksize*1024);
+    coro_create(&mainctx, NULL, NULL, mainstack.sptr, mainstack.ssze);
+    coro_stack_alloc(&stack, stacksize*1024);
     coro_create(&ctx, coro_body, NULL, stack.sptr, stack.ssze);
-    printf("Created a coro\n");
     while(1) {
-      coro_transfer(&mainctx, &ctx);
-      count++;
+        coro_transfer(&mainctx, &ctx);
+        count++;
+        if(count > counter) {
+            break;
+        } 
+        //printf("maincoro:%d\n", count);
     }
-    
-    printf("switch count:%d\n", count);
-    
+    printf("switch count:%d\n", count); 
     return 0;
 }
+
+// gcc coro.c t.c -DCORO_ASM
+// time -p ./a.out 1000000000 8
