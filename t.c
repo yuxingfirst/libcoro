b2b96bb5760cd615cef3c2f9dbe7c24e839d65a1
yuxingfirst <yuxingfirst@gmail.com>
2013-12-23 23:22:51 +0800

    ddd

--- t.c
+++ t.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include "coro.h"
 
-coro_context ctx, mainctx;
-struct coro_stack mainstack, stack;
+coro_context ctx[INX], mainctx;
+
+struct coro_stack mainstack, stack[INX];
+
+int index[INX];
+
 long count = 0;
 void coro_body(void *arg)
 {
+    int i = *(int*)arg;
+
     while(1) {
       count++;
-      coro_transfer(&ctx, &mainctx);
-      //printf("coro:%d\n", count);
+//      printf("coro:%d, count:%ld\n", i, count);
+      if(i == (INX - 1)) {
+            coro_transfer(&ctx[i], &mainctx);
+            continue;
+      }
+      coro_transfer(&ctx[i], &ctx[i+1]);
     }
 }
 
@@ -18,19 +28,26 @@ int main(int argc, char **argv)
     long counter = atol(argv[1]);
     int stacksize = atoi(argv[2]);
     printf("total:%ld, stackszie:%dKB\n", counter, stacksize);
+
     coro_stack_alloc(&mainstack, stacksize*1024);
     coro_create(&mainctx, NULL, NULL, mainstack.sptr, mainstack.ssze);
-    coro_stack_alloc(&stack, stacksize*1024);
-    coro_create(&ctx, coro_body, NULL, stack.sptr, stack.ssze);
+    int i;    
+    for(i=0; i<INX; i++) {
+        index[i] = i;
+        coro_stack_alloc(&stack[i], stacksize*1024);
+        coro_create(&ctx[i], coro_body, &index[i], stack[i].sptr, stack[i].ssze);
+    }
+
     while(1) {
-        coro_transfer(&mainctx, &ctx);
+        coro_transfer(&mainctx, &ctx[0]);
         count++;
+ //       printf("maincoro:%ld\n", count);
         if(count > counter) {
             break;
         } 
         //printf("maincoro:%d\n", count);
     }
-    printf("switch count:%d\n", count); 
+    printf("switch count:%ld\n", count); 
     return 0;
 }
 
